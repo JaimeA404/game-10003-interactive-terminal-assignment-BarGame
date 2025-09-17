dd3427bf8467997ba3e946faf743e4a0f27231a1
JaimeA404 <jaimerafaavan@gmail.com>
2025-09-17 11:46:19 -0400

    Update Program.cs
    added different paths

--- bar game/Program.cs
+++ bar game/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.Design;
 
 class Program
 {
@@ -25,39 +26,219 @@ class Program
             //bad history will be brought up later in the game
 
             Console.WriteLine("");
-            Console.WriteLine("Who do you serve first? (Jerry/Millie/Mrs. Gabriel)");
-            string customer = Console.ReadLine();
+            Console.WriteLine("Who do you serve first?");
+            Console.WriteLine("");
+            Console.WriteLine("1. Jerry");
+            Console.WriteLine("2. Millie");
+            Console.WriteLine("3. Mrs. Gabriel");
+            int customerServed = int.Parse(Console.ReadLine());
 
-            if (customer.ToLower() == "jerry")
+            if (customerServed == 1)
             {
                 Console.WriteLine("");
-                Console.WriteLine("Collect the ingredients for his cactus milkshake with battery acid.");
+                Console.WriteLine("Collect the ingredients for Jerry's cactus milkshake with battery acid.");
+                Console.WriteLine("");
                 Console.WriteLine("[Chocolate Syrup] [Potted Cactus] [Egg Shells] [Pencil Shavings]");
                 Console.WriteLine("[Battery Acid] [Toothpaste] [Soda Water] [Vinegar] [Ice Cream]");
                 Console.WriteLine("[Green Paint] [Armpit Hair] [Shaving Cream] [Milk]");
                 Console.WriteLine("");
                 Console.WriteLine("Select the correct ingredients (separate with commas):");
 
-                string jerryingredients = Console.ReadLine();
+                string jerryIngredients = Console.ReadLine();
 
                 //player must choose the correct ingredients in the right sequence
 
-                bool jerrydrinkcorrect = jerryingredients.ToLower() == "potted cactus, battery acid, ice cream, milk";
+                bool jerryDrinkcorrect = jerryIngredients.ToLower() == "potted cactus, battery acid, ice cream, milk";
 
-                if (jerrydrinkcorrect)
+                if (jerryDrinkcorrect)
                 {
                     Console.WriteLine("");
                     Console.WriteLine("You served Jerry his milkshake.");
                     Console.WriteLine("Jerry: Ah, just like how mama used to make it.");
                     Console.WriteLine("");
-                    Console.WriteLine("Who will you serve now? (Millie/Mrs. Gabriel)");
+                    Console.WriteLine("Who will you serve now?");
+                    Console.WriteLine("");
+                    Console.WriteLine("2. Millie");
+                    Console.WriteLine("3. Mrs. Gabriel");
+                    customerServed = int.Parse(Console.ReadLine());
+
+                    if (customerServed == 2)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Collect the ingredients for Millie's garbage juice.");
+                        Console.WriteLine("");
+                        Console.WriteLine("[Banana Peel] [Orange Puree] [Pineapple Juice] [Candy]");
+                        Console.WriteLine("[Hand Sanitizer] [Used Socks] [Cup of Pee] [Cheese]");
+                        Console.WriteLine("Snotty Tissue] [Garbage Water] [Stawberries] [Vomit]");
+                        Console.WriteLine("");
+                        Console.WriteLine("Select the correct ingredients (separate with commas):");
+
+                        string millieIngredients = Console.ReadLine();
+
+                        bool millieDrinkcorrect = millieIngredients.ToLower() == "banana peel, used socks, cup of pee, snotty tissue, garbage water, vomit";
+
+                        if (millieDrinkcorrect)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("you served Millie her garbage juice.");
+                            Console.WriteLine("Millie: This is the best garbage juice I've ever had!");
+                            Console.WriteLine("");
+                            Console.WriteLine("Only Mrs. Gabriel is left to serve.");
+                            Console.WriteLine("");
+                            Console.WriteLine("Collect the ingredients for Mrs. Gabriel's glass of water with lots of ice.");
+                            Console.WriteLine("");
+                            Console.WriteLine("      [Ice Cubes]                  [Tap Water]     ");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                            Console.WriteLine("                      [Poison]                     ");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                            Console.WriteLine("Select the correct ingredients (separate with commas):");
+                            string gabrielIngredients = Console.ReadLine();
+
+                            bool gabrieldrinkPoisoned = gabrielIngredients.ToLower() == "ice cubes, tap water, poison";
+
+                            if (gabrieldrinkPoisoned)
+                            {
+                                Console.WriteLine("");
+                                Console.WriteLine("You served Mrs. Gabriel her glass of water with lots of ice.");
+                                Console.WriteLine("Mrs. Gabriel: This water tastes funny...");
+                                Console.WriteLine("Mrs. Gabriel suddenly collapses to the floor.");
+                                Console.WriteLine("");
+                                Console.WriteLine("You poisoned your high school teacher who you never liked.");
+                                Console.WriteLine("The crowd builds up around her.");
+                                Console.WriteLine("It didn't take long for the police to show up");
+                                Console.WriteLine("You were arrested for poisoning Mrs. Gabriel.");
+                                Console.WriteLine("But in your world, you don't care because you never liked her.");
+                                Console.WriteLine("");
+                                Console.WriteLine("You win!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("");
+                                Console.WriteLine("You served Mrs. Gabriel her glass of water with lots of ice.");
+                                Console.WriteLine("Mrs. Gabriel: This water is too cold!");
+                                Console.WriteLine("");
+                                Console.WriteLine("Ungrateful b!#*h");
+                                Console.WriteLine("You are unsatisfied.");
+                                Console.WriteLine("");
+                                Console.WriteLine("You . . .");
+                                Console.WriteLine("Win?");
+                            }
+
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Millie: Something's missing... I'm Leaving!");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Jerry: This isn't right... I'm leaving!");
+                    }
                 }
-                else
+                else if (customerServed == 2)
                 {
                     Console.WriteLine("");
-                    Console.WriteLine("Jerry: This isn't right... I'm leaving!");
+                    Console.WriteLine("Collect the ingredients for her garbage juice.");
+                    Console.WriteLine("");
+                    Console.WriteLine("[Banana Peel] [Orange Puree] [Pineapple Juice] [Candy]");
+                    Console.WriteLine("[Hand Sanitizer] [Used Socks] [Cup of Pee] [Cheese]");
+                    Console.WriteLine("Snotty Tissue] [Garbage Water] [Stawberries] [Vomit]");
+                    Console.WriteLine("");
+                    Console.WriteLine("Select the correct ingredients (separate with commas):");
+                    string millieIngredients2 = Console.ReadLine();
+                    bool millieDrinkcorrect2 = millieIngredients2.ToLower() == "banana peel, used socks, cup of pee, snotty tissue, garbage water, vomit";
+                    if (millieDrinkcorrect2)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("you served Millie her garbage juice.");
+                        Console.WriteLine("Millie: This is the best garbage juice I've ever had!");
+                        Console.WriteLine("");
+                        Console.WriteLine("Who will you serve now?");
+                        Console.WriteLine("");
+                        Console.WriteLine("1. Jerry");
+                        Console.WriteLine("3. Mrs. Gabriel");
+                        customerServed = int.Parse(Console.ReadLine());
+                        if (customerServed == 1)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Collect the ingredients for Jerry's cactus milkshake with battery acid.");
+                            Console.WriteLine("");
+                            Console.WriteLine("[Chocolate Syrup] [Potted Cactus] [Egg Shells] [Pencil Shavings]");
+                            Console.WriteLine("[Battery Acid] [Toothpaste] [Soda Water] [Vinegar] [Ice Cream]");
+                            Console.WriteLine("[Green Paint] [Armpit Hair] [Shaving Cream] [Milk]");
+                            Console.WriteLine("");
+                            Console.WriteLine("Select the correct ingredients (separate with commas):");
+                            string jerryIngredients2 = Console.ReadLine();
+                            bool jerrydrinkCorrect2 = jerryIngredients2.ToLower() == "potted cactus, battery acid, ice cream, milk";
+                            if(jerrydrinkCorrect2)
+                            {
+                                Console.WriteLine("");
+                                Console.WriteLine("You served Jerry his milkshake.");
+                                Console.WriteLine("Jerry: Ah, just like how mama used to make it.");
+                                Console.WriteLine("");
+                                Console.WriteLine("Only Mrs. Gabriel is left to serve.");
+                                Console.WriteLine("");
+                                Console.WriteLine("Collect the ingredients for Mrs. Gabriel's glass of water with lots of ice.");
+                                Console.WriteLine("");
+                                Console.WriteLine("      [Ice Cubes]                  [Tap Water]     ");
+                                Console.WriteLine("");
+                                Console.WriteLine("");
+                                Console.WriteLine("                      [Poison]                     ");
+                                Console.WriteLine("");
+                                Console.WriteLine("");
+                                Console.WriteLine("Select the correct ingredients (separate with commas):");
+                                string gabrielIngredients2 = Console.ReadLine();
+                                bool gabrieldrinkPoisoned2 = gabrielIngredients2.ToLower() == "ice cubes, tap water, poison";
+                                if ( gabrieldrinkPoisoned2)
+                                    {
+                                    Console.WriteLine("");
+                                    Console.WriteLine("You served Mrs. Gabriel her glass of water with lots of ice.");
+                                    Console.WriteLine("Mrs. Gabriel: This water tastes funny...");
+                                    Console.WriteLine("Mrs. Gabriel suddenly collapses to the floor.");
+                                    Console.WriteLine("");
+                                    Console.WriteLine("You poisoned your high school teacher who you never liked.");
+                                    Console.WriteLine("The crowd builds up around her.");
+                                    Console.WriteLine("It didn't take long for the police to show up");
+                                    Console.WriteLine("You were arrested for poisoning Mrs. Gabriel.");
+                                    Console.WriteLine("But in your world, you don't care because you never liked her.");
+                                    Console.WriteLine("");
+                                    Console.WriteLine("You win!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("");
+                                    Console.WriteLine("You served Mrs. Gabriel her glass of water with lots of ice.");
+                                    Console.WriteLine("Mrs. Gabriel: This water is too cold!");
+                                    Console.WriteLine("");
+                                    Console.WriteLine("Ungrateful b!#*h");
+                                    Console.WriteLine("You are unsatisfied.");
+                                    Console.WriteLine("");
+                                    Console.WriteLine("You . . .");
+                                    Console.WriteLine("Win?");
+                                }
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Millie: Something's missing... I'm Leaving!");
+                    }
+                }
+                else if (customerServed == 3)
+                {
+
                 }
             }
+          
         }
         else if (answer.ToLower() == "no") //finally moved the "no" option to the proper place
         {
